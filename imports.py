5dd662c38fb641204bcf597f3c69eccec0dfcfaf
AdmiralScoob <thatguyscoob1@gmail.com>
2024-06-09 12:23:47 -0400

    Added mmicrobit library and moved imports into seperate file to tidy things up.

---
+++ imports.py
@@ -0,0 +1,10 @@
+import microbit
+from microbit import basic
+from microbit import Button
+from microbit import DrawScreen
+from microbit import led
+from microbit import loops
+from microbit import Math
+from microbit import radio
+from microbit import RadioPacketProperty
+from microbit import IconNames
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import microbit
+from imports import *
 
-microbit.radio.set_group(1)      # the radio frequency we will be using, leave on 1
-myID = 4               # The ID # given to you by the teacher
+radio.set_group(1)      # the radio frequency we will be using, leave on 1
+myID = 4                # The ID # given to you by the teacher
 signalStrength = -999   # keeps track of the signal strength to let you know when you dont have a signal anymore
 hasSignal = False       # this flag will be true when you are receiving a signal
 wasFound = False        # this flag will be true when you have found the transmitter
@@ -12,35 +12,35 @@ showNum = False         # this flag with be true when displaying the signal stre
 def on_received_number(receivedNumber):
     global hasSignal, signalStrength, wasFound
     # the transmitter will send a 0 every 500ms
-    if microbit.radio.received_number == 0:
+    if radio.received_number == 0:
         hasSignal = True
-        signalStrength = microbit.radio.received_packet(microbit.RadioPacketProperty.SIGNAL_STRENGTH)
+        signalStrength = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
     # the transmitter will echo back your ID when it records it as found
     elif receivedNumber == myID:
         wasFound = True
-microbit.radio.on_received_number(on_received_number)
+radio.on_received_number(on_received_number)
 
 # for debugging, will display the signal strength value
 def on_button_pressed_a():
     global showNum
     showNum = not showNum
-input.on_button_pressed(microbit.Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 # transmit your ID to the transmitter
 def on_button_pressed_b():
-    microbit.radio.send_number(myID)
-input.on_button_pressed(microbit.Button.B, on_button_pressed_b)
+    radio.send_number(myID)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 # draws the bar graph and signal lost / winner icons
 def drawScreen():
     if wasFound:
-        microbit.basic.show_icon(microbit.IconNames.HAPPY)
+        basic.show_icon(IconNames.HAPPY)
     elif signalStrength == -999:
-        microbit.basic.show_icon(microbit.IconNames.NO)
+        basic.show_icon(IconNames.NO)
     elif showNum:
-        microbit.basic.show_number(signalStrength)
+        basic.show_number(signalStrength)
     else:
-        microbit.led.plot_bar_graph(microbit.Math.map(signalStrength, -128, -28, 0, 9), 9)
+        led.plot_bar_graph(Math.map(signalStrength, -128, -28, 0, 9), 9)
 
 # heartbeat, checks that we still have a signal, otherwise will display that we lost it
 def on_every_interval():
@@ -49,9 +49,9 @@ def on_every_interval():
         hasSignal = False
     else:
         signalStrength = -999
-microbit.loops.every_interval(1000, on_every_interval)
+loops.every_interval(1000, on_every_interval)
 
 def on_forever():
-    microbit.DrawScreen()
+    DrawScreen()
     on_every_interval()
-microbit.basic.forever(on_forever)
+basic.forever(on_forever)
